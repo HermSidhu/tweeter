b33c0d4ca589d9c15e60cdd22776ce110a4ad81e
HermSidhu <h-sidhu@hotmail.com>
2019-11-07 18:00:11 +0000

    posting and getting tweets from server implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,61 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) { // loops through tweets
+    let $tweet = createTweetElement(tweet) // calls createTweetElement for each tweet
+    $("#tweetContainer").prepend($tweet) // takes return value and appends it to the tweets container
+  }
+}
+
+const createTweetElement = function (tweet) {
+  let $tweet = ` <article class="tweet">
+                <header>
+                  
+                    <img src=${tweet.user.avatars}>
+                    <span class="tweeterName">${tweet.user.name}</span>
+                    <span class="username">${tweet.user.handle}</span>
+                  
+                </header>
+
+                <div class="sampleTweetText">
+                  <p>${tweet.content.text}</p>
+                </div>
+
+                <footer>
+                    <div>
+                      <span>${tweet.created_at}</span>
+                      <span>
+                        <i class="fas fa-flag dateIcons"></i>
+                        <i class="fas fa-retweet"></i>
+                        <i class="fas fa-heart"></i>
+                      </span>  
+                      </div>
+                </footer>
+              </article> `
+
+  return $tweet;
+}
+
+const loadTweets = function () {
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function (res) {
+    renderTweets(res);  
+  })
+}
+
+$(document).ready( function() {
+  loadTweets();
+
+  const $form = $("#new-tweet-form");
+  $form.on('submit', function () {
+    event.preventDefault()
+    let formData = $form.serialize()
+    $.ajax('/tweets', { method: 'POST' , data: formData})
+    .then(function (res) { 
+      loadTweets();
+    })
+  })
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
   $('#tweet-box').on("keyup", function () {
+    const MAX_CHAR = 140
     let charCount = $(this).val().length;
-    $('#counter').text(140 - charCount);
-    if (charCount > 140) {
+    $('#counter').text(MAX_CHAR - charCount);
+    if (charCount > MAX_CHAR) {
       $("#counter").css("color", "red");
     } else $("#counter").css("color", "black")
   });
